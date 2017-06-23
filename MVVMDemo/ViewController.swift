a19dfd4e3d258d7ffa485b3b646f7d11af5c31a4
Grubby <goreyjp@qq.com>
2017-06-23 17:09:30 +0800

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by Gorey on 2017/6/23.
+//  Copyright © 2017年 GG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
